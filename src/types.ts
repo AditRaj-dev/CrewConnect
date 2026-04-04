88f45c7a4a1176d5299159ba85da1b7417efa55a
AditRaj-dev <louis05022006@gmail.com>
2026-04-04 13:21:17 +0530

    feat: add TypeScript type definitions

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export interface Feature {
+  id: string
+  title: string
+  description: string
+  icon: string
+}
+
+export interface Screenshot {
+  id: string
+  image: string
+  caption: string
+}
+
+export interface CarouselSlide {
+  id: string
+  image: string
+  alt: string
+  caption: string
+}
